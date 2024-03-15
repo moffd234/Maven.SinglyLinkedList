9415aa7713304cf936ac343b27c1b38b9930083e
moffd234 <moffd234@gmail.com>
2024-03-15 19:50:19 -0400

    Created Method signatures for the test class

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,29 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import javax.xml.soap.Node;
+import java.util.AbstractSequentialList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<DataType> extends AbstractSequentialList<DataType>
+                                        implements List<DataType> {
+    int size = 0;
+
+    SinglyLinkedList(){
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<DataType> listIterator(int index) {
+        return null;
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,52 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+
+import org.junit.Test;
+
+import java.util.List;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    @Test
+    public void testAdd(){
+        List<String> linkedList = new SinglyLinkedList<>();
+    }
+    @Test
+    public void testRemove(){
+        List<String> linkedList = new SinglyLinkedList<>();
+    }
+    @Test
+    public void testContains(){
+        List<String> linkedList = new SinglyLinkedList<>();
+    }
+    @Test
+    public void testFind(){
+        List<String> linkedList = new SinglyLinkedList<>();
+    }
+    @Test
+    public void testSize(){
+        List<String> linkedList = new SinglyLinkedList<>();
+    }
+    @Test
+    public void testGet(){
+        List<String> linkedList = new SinglyLinkedList<>();
+    }
+    @Test
+    public void testCopy(){
+        List<String> linkedList = new SinglyLinkedList<>();
+    }
+    @Test
+    public void testSort(){
+        List<String> linkedList = new SinglyLinkedList<>();
+    }
+    @Test
+    public void testReverse(){
+        List<String> linkedList = new SinglyLinkedList<>();
+    }
+    @Test
+    public void testSlice(){
+        List<String> linkedList = new SinglyLinkedList<>();
+    }
 }
