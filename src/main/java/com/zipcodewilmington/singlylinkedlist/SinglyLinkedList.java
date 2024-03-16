ad1a74f51548fa40eb55be263b834edb8498f1da
moffd234 <moffd234@gmail.com>
2024-03-15 20:12:56 -0400

    Created a Node constructor and class

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,29 +1,40 @@
 package com.zipcodewilmington.singlylinkedlist;
 
 import javax.xml.soap.Node;
-import java.util.AbstractSequentialList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList<DataType> extends AbstractSequentialList<DataType>
-                                        implements List<DataType> {
-    int size = 0;
+                                        implements List<DataType>{
+    private int size = 0;
+    Node<DataType> head;
+    Node<DataType> tail;
 
     SinglyLinkedList(){
-
+         head = new Node<DataType>(null, null); // Declares the head to be null
+                                                             // since no head was provided
+         tail = head; // Sets tail to head since there isn't more than one item
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public ListIterator<DataType> listIterator(int index) {
         return null;
+    }
+
+    private static class Node<DataType> {
+        DataType item;
+        Node<DataType> next;
+
+        Node(DataType element, Node<DataType> next) {
+            this.item = element;
+            this.next = next;
+        }
     }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,6 +1,7 @@
 package com.zipcodewilmington.singlylinkedlist;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
@@ -11,42 +12,94 @@ import java.util.List;
 public class SinglyLinkedListTest {
     @Test
     public void testAdd(){
+        // Given
         List<String> linkedList = new SinglyLinkedList<>();
+
+        // When
+        String toAdd = "This has been added";
+        linkedList.add(toAdd);
+
+
+        // Then
+        Assert.assertTrue(linkedList.contains(toAdd));
     }
     @Test
     public void testRemove(){
         List<String> linkedList = new SinglyLinkedList<>();
+
+        // When
+
+        // Then
     }
     @Test
     public void testContains(){
+        // Given
         List<String> linkedList = new SinglyLinkedList<>();
+
+        // When
+        String toAdd = "This has been added";
+        String addThis = "This has also been added";
+        linkedList.add(toAdd);
+
+
+        // Then
+        Assert.assertTrue(linkedList.contains(toAdd));
+        Assert.assertTrue(linkedList.contains(addThis));
     }
     @Test
     public void testFind(){
         List<String> linkedList = new SinglyLinkedList<>();
+
+        // When
+
+        // Then
     }
     @Test
     public void testSize(){
         List<String> linkedList = new SinglyLinkedList<>();
+
+        // When
+
+        // Then
     }
     @Test
     public void testGet(){
         List<String> linkedList = new SinglyLinkedList<>();
+
+        // When
+
+        // Then
     }
     @Test
     public void testCopy(){
         List<String> linkedList = new SinglyLinkedList<>();
+
+        // When
+
+        // Then
     }
     @Test
     public void testSort(){
         List<String> linkedList = new SinglyLinkedList<>();
+
+        // When
+
+        // Then
     }
     @Test
     public void testReverse(){
         List<String> linkedList = new SinglyLinkedList<>();
+
+        // When
+
+        // Then
     }
     @Test
     public void testSlice(){
         List<String> linkedList = new SinglyLinkedList<>();
+
+        // When
+
+        // Then
     }
 }
